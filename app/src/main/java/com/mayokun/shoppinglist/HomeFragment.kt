17a26267a5ad695859ea3d65631c0cc743d0541f
mayokunthefirst <adeniyimayokun17@gmail.com>
2019-11-08 23:24:13 +0100

    feat: add home fragment

---
+++ app/src/main/java/com/mayokun/shoppinglist/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.mayokun.shoppinglist
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.mayokun.shoppinglist.databinding.FragmentHomeBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+       val binding: FragmentHomeBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_home
+       ,container,false)
+
+
+        return binding.root
+
+    }
+
+
+}
