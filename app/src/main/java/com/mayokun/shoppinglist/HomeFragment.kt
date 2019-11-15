217896ea8be09d5ce4f10e1cecd29d607be84709
mayokunthefirst <adeniyimayokun17@gmail.com>
2019-11-15 17:20:58 +0100

    feat: add dialog fragment for new item

--- app/src/main/java/com/mayokun/shoppinglist/HomeFragment.kt
+++ app/src/main/java/com/mayokun/shoppinglist/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.mayokun.shoppinglist.databinding.FragmentHomeBinding
+import com.mayokun.shoppinglist.utils.NewItemFragment
 
 /**
  * A simple [Fragment] subclass.
@@ -23,8 +24,22 @@ class HomeFragment : Fragment() {
             , container, false
         )
 
+        binding.newItemButton.setOnClickListener { createPopUp() }
         return binding.root
+    }
 
+    /**
+     * This shows the custom dialog for adding a new item
+     */
+    private fun createPopUp() {
+        val newItemFragment = NewItemFragment()
+        val fragmentTransaction = fragmentManager?.beginTransaction()
+        val prev = fragmentManager?.findFragmentByTag("NewItem")
+        if (prev != null) {
+            fragmentTransaction?.remove(prev)
+        }
+        fragmentTransaction?.addToBackStack(null)
+        newItemFragment.show(fragmentTransaction!!,"NewItem")
     }
 
 }
